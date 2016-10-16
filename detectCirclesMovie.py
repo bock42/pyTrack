7222ab0da9638abff288fac1481e4944790228bb
bock42 <bock.andrew@gmail.com>
2016-10-15 22:42:35 -0400

    [ENH] only keep glint if inside pupil

--- detectCirclesMovie.py
+++ detectCirclesMovie.py
@@ -1,56 +1,60 @@
 import cv2
 import numpy
+import math
 
+# load the eye tracking video
 cap = cv2.VideoCapture('/home/abock/videos/eyeMovie.mov')
 
+# loop through the frames
 while(cap.isOpened()):
+	# get the frame, save output for later
     	ret, frame = cap.read()
-	
 	output = frame.copy();
 
+	# convert to gray
     	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+	# blur the image
 	blur = cv2.medianBlur(gray,7)
 
+	# binarize
 	ret,thresh = cv2.threshold(blur,200,255,cv2.THRESH_BINARY)
 
+	# get the pupil
 	circlesPupil = cv2.HoughCircles(blur, cv2.cv.CV_HOUGH_GRADIENT, 1, 10000,minRadius=15,maxRadius=100,param1=100,param2=10)
 
+	# get the glint
 	circlesGlint = cv2.HoughCircles(thresh, cv2.cv.CV_HOUGH_GRADIENT, 1, 10000,minRadius=1,maxRadius=10,param1=100,param2=10)
 
-	# ensure at least some circles were found
+	# ensure the pupil was found
 	if circlesPupil is not None:
 		# convert the (x, y) coordinates and radius of the circles to integers
 		circlesPupil = numpy.round(circlesPupil[0, :]).astype("int")
  
-		# loop over the (x, y) coordinates and radius of the circles
-		for (x, y, r) in circlesPupil:
-			# draw the circle in the output image, then draw a rectangle
-			# corresponding to the center of the circle
-			cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-			#cv2.circle(output, (x, y), 1, (0, 128, 255), -1)
-			#cv2.rectangle(output, (x - 2, y - 2), (x + 2, y + 2), (0, 128, 255), -1)
+		# draw the pupil 
+		for (xP, yP, rP) in circlesPupil:
+			cv2.circle(output, (xP, yP), rP, (0, 255, 0), 4)
 
-	# ensure at least some circles were found
+	# ensure the glint was found
 	if circlesGlint is not None:
 		# convert the (x, y) coordinates and radius of the circles to integers
 		circlesGlint = numpy.round(circlesGlint[0, :]).astype("int")
  
-		# loop over the (x, y) coordinates and radius of the circles
-		for (x, y, r) in circlesGlint:
-			# draw the circle in the output image, then draw a rectangle
-			# corresponding to the center of the circle
-			cv2.circle(output, (x, y), r, (0, 0, 255), 4)
-			#cv2.circle(output, (x, y), 1, (0, 128, 255), -1)
-			#cv2.rectangle(output, (x - 2, y - 2), (x + 2, y + 2), (0, 128, 255), -1)
+		# draw the glint
+		for (xG, yG, rG) in circlesGlint:
+			# only if glint is inside the pupil
+			if math.sqrt( pow((xP - xG),2) + pow((yP - yG),2) ) < rP:
+				cv2.circle(output, (xG, yG), rG, (0, 0, 255), 4)
 
 	# show the output image
 	#cv2.imshow("output", numpy.hstack([frame, output]))
 	cv2.imshow('output',output)
 
-
+	# quit movie if "q" is pressed
     	if cv2.waitKey(33) & 0xFF == ord('q'):
         	break
+
+# close the windows
 cap.release()
 cv2.destroyAllWindows()
 
