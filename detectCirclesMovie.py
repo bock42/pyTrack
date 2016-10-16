f655d35e919917c397f1d1735862c1e1e3a49cad
bock42 <bock.andrew@gmail.com>
2016-10-16 00:28:00 -0400

    [ENH] dynamically adjust HoughCircles param2 value

--- detectCirclesMovie.py
+++ detectCirclesMovie.py
@@ -3,8 +3,13 @@ import numpy
 import math
 
 # set defaults
-radPupil = [25,100]
-radGlint = [3,10]
+radP = [25,100]
+radG = [3,10]
+p1P = 500
+p1G = 500
+p2P = 35
+p2G = 10
+maxp2P = 45
 
 # load the eye tracking video
 cap = cv2.VideoCapture('/home/abock/videos/eyeMovie.mov')
@@ -18,27 +23,29 @@ while(cap.isOpened()):
 	# convert to gray
     	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	# blur the image
-	blur = cv2.GaussianBlur(gray,(5,5),20)
-
 	# binarize
-	ret,thresh = cv2.threshold(blur,50,255,cv2.THRESH_BINARY)
+	ret,thresh = cv2.threshold(gray,50,255,cv2.THRESH_BINARY)
+
+	# blur the image
+	blur = cv2.GaussianBlur(thresh,(7,7),20)
 
 	# get the pupil
-	circlesPupil = cv2.HoughCircles(blur, cv2.cv.CV_HOUGH_GRADIENT, 1, 10000,minRadius=radPupil[0],maxRadius=radPupil[1],param1=100,param2=10)
+	circlesPupil = cv2.HoughCircles(blur, cv2.cv.CV_HOUGH_GRADIENT, 1, 10000,minRadius=radP[0],maxRadius=radP[1],param1=p1P,param2=p2P)
 
 	# get the glint
-	circlesGlint = cv2.HoughCircles(blur, cv2.cv.CV_HOUGH_GRADIENT, 1, 10000,minRadius=radGlint[0],maxRadius=radGlint[1],param1=100,param2=10)
+	circlesGlint = cv2.HoughCircles(blur, cv2.cv.CV_HOUGH_GRADIENT, 1, 10000,minRadius=radG[0],maxRadius=radG[1],param1=p1G,param2=p2G)
 
 	# ensure the pupil was found
 	if circlesPupil is not None:
 		# convert the (x, y) coordinates and radius of the circles to integers
 		circlesPupil = numpy.round(circlesPupil[0, :]).astype("int")
+		if p2P < maxp2P:
+			p2P = numpy.round(p2P*1.05).astype("int")
  
 		# draw the pupil 
 		for (xP, yP, rP) in circlesPupil:
 			cv2.circle(output, (xP, yP), rP, (0, 255, 0), 4)
-			radPupil = numpy.round([rP*0.85,rP*1.15]).astype("int")
+			radP = numpy.round([rP*0.85,rP*1.15]).astype("int")
 
 		# ensure the glint was found
 		if circlesGlint is not None:
@@ -50,6 +57,9 @@ while(cap.isOpened()):
 				# only if glint is inside the pupil
 				if math.sqrt( pow((xP - xG),2) + pow((yP - yG),2) ) < rP:
 					cv2.circle(output, (xG, yG), rG, (0, 0, 255), 4)
+	else:
+		# lower the param2 threshold
+		p2P = numpy.round(p2P*0.95).astype("int")
 
 	# show the output image
 	cv2.imshow("output", numpy.hstack([frame, output]))
